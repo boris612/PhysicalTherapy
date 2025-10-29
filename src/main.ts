5a9912051e9ca634cebd8fddbaffe864b719826e
FraneKuzmanic <frane.kuzmanic9@gmail.com>
2025-10-29 01:10:11 +0100

    auth0-bugfix

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ app.use(
     domain: import.meta.env.VITE_AUTH0_DOMAIN || 'YOUR_AUTH0_DOMAIN',
     clientId: import.meta.env.VITE_AUTH0_CLIENT_ID || 'YOUR_AUTH0_CLIENT_ID',
     authorizationParams: {
-      redirect_uri: window.location.origin 
+      redirect_uri: window.location.origin + '/p2025'
     }
   })
 );
